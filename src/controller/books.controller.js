c181b1f7f024e794f93a319b23c6a26f62bd69e9
Your Name <you@example.com>
2024-06-27 15:04:26 -0300

    Se terminó la funcionalidad de eliminar un libro

--- src/controller/books.controller.js
+++ src/controller/books.controller.js
@@ -1,3 +1,4 @@
+const { title } = require("process");
 const database = require("../bd");
 const ctrl = {}
 
@@ -5,6 +6,13 @@ const viewBook=(req, res)=>{
     res.json(database)
 }
 
+const deleteBook =(req, res)=>{
+    const {id} = parseInt(req.params.id)
+    const buscar = database.find((title)=>title.id===id)
+    const indice = database.indexOf(buscar)
+    const borrar = database.splice(indice, 1)
+    res.json({mensaje:"libro eliminado", borrar})
+}
 
-module.exports={viewBook}
 
+module.exports={viewBook, deleteBook}
--- src/routes/books.router.js
+++ src/routes/books.router.js
@@ -1,8 +1,10 @@
-const {viewBook} = require("../controller/books.controller");
+const {viewBook, deleteBook} = require("../controller/books.controller");
 const express = require("express")
 
 const router = express.Router()
 
 router.get("/book", viewBook)
+
+router.delete("/book/delete/:id", deleteBook)
 
 module.exports=router
