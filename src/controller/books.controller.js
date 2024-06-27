b837ffcabe05c59462f32af9f6d37c27dd2577b5
Your Name <you@example.com>
2024-06-27 16:07:08 -0300

    se terminó el parcial

--- src/controller/books.controller.js
+++ src/controller/books.controller.js
@@ -1,56 +1,56 @@
 const database = require("../bd");
 
-const viewBook=(req, res)=>{
+const viewBook = (req, res) => {
     res.json(database)
 }
 
-const deleteBook =(req, res)=>{
-    const {id} = parseInt(req.params.id)
-    const buscar = database.find((title)=>title.id===id)
+const deleteBook = (req, res) => {
+    const { id } = parseInt(req.params.id)
+    const buscar = database.find((title) => title.id === id)
     const indice = database.indexOf(buscar)
     const borrar = database.splice(indice, 1)
-    res.json({mensaje:"libro eliminado", borrar})
+    res.json({ mensaje: "libro eliminado", borrar })
 }
 
-const insertBook = (req, res)=>{
-    let id = database.length + 1; 
-    const {title, author, year} = req.body;
-    
-    !title || !author || !year ? res.json({ message: "Todos los campos son obligatorios" }) : database.push({ id: id, title:title, author:author, year:year });
-    res.json({mensaje:"libro agregado"})
+const insertBook = (req, res) => {
+    let id = database.length + 1;
+    const { title, author, year } = req.body;
+
+    !title || !author || !year ? res.json({ message: "Todos los campos son obligatorios" }) : database.push({ id: id, title: title, author: author, year: year });
+    res.json({ mensaje: "libro agregado" })
 
 }
 
-const modifyBook = (req, res)=>{
-    const {title, author, year} = req.body
+const modifyBook = (req, res) => {
+    const { title, author, year } = req.body
     const id = parseInt(req.params.id)
 
-    const buscar = database.find((book)=>book.id===id);
-    buscar.title=title;
-    buscar.author=author;
-    buscar.year=year;
+    const buscar = database.find((book) => book.id === id);
+    buscar.title = title;
+    buscar.author = author;
+    buscar.year = year;
 
-    res.json({mensaje:"libro agregado"})
+    res.json({ mensaje: "libro agregado" })
 }
 
-const selectBookId = (req, res)=>{
+const selectBookId = (req, res) => {
     try {
-    const id = parseInt( req.params.id)
-    
-    const buscar = database.find((title)=>title.id===id)
-    
-    if(!buscar){
-        res.json({mensaje:"libro no encontrado"})
-    }
-        
-    res.json(buscar);
+        const id = parseInt(req.params.id)
+
+        const buscar = database.find((title) => title.id === id)
+
+        if (!buscar) {
+            res.json({ mensaje: "libro no encontrado" })
+        }
+
+        res.json(buscar);
 
 
     } catch (error) {
-        res.json({mensaje:"error"})
+        res.json({ mensaje: "error" })
     }
 
 }
 
-module.exports={viewBook, deleteBook, insertBook, modifyBook, selectBookId}
+module.exports = { viewBook, deleteBook, insertBook, modifyBook, selectBookId, insertById }
 
--- src/routes/books.router.js
+++ src/routes/books.router.js
@@ -1,4 +1,4 @@
-const {viewBook, deleteBook, insertBook, modifyBook, selectBookId} = require("../controller/books.controller");
+const {viewBook, deleteBook, insertBook, modifyBook, selectBookId, insertById} = require("../controller/books.controller");
 const express = require("express")
 
 const router = express.Router()
@@ -12,5 +12,7 @@ router.post("/book/insert", insertBook)
 router.put("/book/update/:id", modifyBook)
 
 router.get("/book/:id", selectBookId)
+
+router.post("/book/insertId/:id", insertById)
 
 module.exports=router
